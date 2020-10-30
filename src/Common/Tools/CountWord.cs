8d0d0f0ab6ec2796bf64cab97e3ccf69979fccbc
Mitiko <mitikodev@gmail.com>
2020-10-30 17:04:17 +0200

    Add a custom word counter tool

---
+++ src/Common/Tools/CountWord.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using BWDPerf.Interfaces;
+
+namespace BWDPerf.Common.Tools
+{
+    public class CountWord : ICoder<byte, byte>
+    {
+        private byte[] BytesToMatch { get; }
+        public string Word { get; }
+        public int Count { get; private set; }
+
+        public CountWord(string word, Encoding encoding = null)
+        {
+            if (encoding == null) encoding = Encoding.UTF8;
+            this.BytesToMatch = encoding.GetBytes(word);
+            this.Word = word;
+        }
+
+        public async IAsyncEnumerable<byte> Encode(IAsyncEnumerable<byte> input)
+        {
+            int index = 0;
+            await foreach (var symbol in input)
+            {
+                if (index == this.BytesToMatch.Length)
+                {
+                    this.Count++;
+                    index = 0;
+                }
+                index = this.BytesToMatch[index] == symbol ? index + 1 : 0;
+
+                yield return symbol;
+            }
+
+            Console.WriteLine($"[{this.GetHashCode()}] The word \"{this.Word}\" was counted {this.Count} times");
+        }
+    }
+}
