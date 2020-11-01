579b11fac02488c649878518490cbbb9c7e87cc7
Mitiko <mitikodev@gmail.com>
2020-11-01 02:00:39 +0200

    Make entropy measurable on a sequence of dictionary indices (such as after BWD)

--- src/Common/Tools/MeasureEntropy.cs
+++ src/Common/Tools/MeasureEntropy.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Threading.Channels;
 using System.Threading.Tasks;
 using BWDPerf.Architecture;
+using BWDPerf.Common.Entities;
 using BWDPerf.Interfaces;
 using BWDPerf.Tools;
 
 namespace BWDPerf.Common.Tools
 {
-    public class MeasureEntropy : ICoder<byte, byte>
+    public class MeasureEntropy : ICoder<byte, byte>, ICoder<DictionaryIndex, byte>
     {
         private OccurenceDictionary<byte> OD { get; } = new();
 
@@ -27,6 +28,25 @@ namespace BWDPerf.Common.Tools
                 .Select(x => - Math.Log2(x) * x)
                 .Sum();
             Console.WriteLine($"[{this.GetHashCode()}] Entropy: {entropy}");
+        }
+
+        public async IAsyncEnumerable<byte> Encode(IAsyncEnumerable<DictionaryIndex> input)
+        {
+            int count = 0;
+            await foreach (var index in input)
+            {
+                byte symbol = (byte) index.Index;
+                this.OD.Add(symbol);
+                count++;
+                yield return symbol;
+            }
+
+            double total = this.OD.Sum();
+            var entropy = this.OD.Values
+                .Select(x => x / total)
+                .Select(x => - Math.Log2(x) * x)
+                .Sum();
+            Console.WriteLine($"[{this.GetHashCode()}] Entropy: {entropy}, Count: {count}");
         }
     }
 }
