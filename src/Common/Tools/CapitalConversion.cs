724f0fc1f8ffbfa8469d714e96a1d9513a305247
Mitiko <mitikodev@gmail.com>
2020-11-01 02:00:10 +0200

    Add ability of capital conversion pre-processing to code in blocks.

--- src/Common/Tools/CapitalConversion.cs
+++ src/Common/Tools/CapitalConversion.cs
@@ -8,7 +8,7 @@ using System;
 
 namespace BWDPerf.Common.Tools
 {
-    public class CapitalConversion : ICoder<byte, byte>, IDecoder<byte, byte>
+    public class CapitalConversion : ICoder<byte, byte>, ICoder<byte[], byte[]>, IDecoder<byte, byte>
     {
         public Decoder Decoder { get; }
         public Encoder Encoder { get; }
@@ -60,6 +60,53 @@ namespace BWDPerf.Common.Tools
                         break;
                     }
                 }
+            }
+            this.Decoder.Reset();
+            this.Encoder.Reset();
+        }
+
+        public async IAsyncEnumerable<byte[]> Encode(IAsyncEnumerable<byte[]> input)
+        {
+            byte[] buffer = new byte[1];
+            byte[] lowerCaseBytes = new byte[this.MaxByteCount];
+            char[] charBuffer = new char[this.MaxByteCount];
+            var skippedBytes = new Queue<byte>();
+            await foreach (var block in input)
+            {
+                var list = new List<byte>();
+                foreach (var symbol in block)
+                {
+                    buffer[0] = symbol;
+                    var written = this.Decoder.GetChars(buffer, 0, 1, charBuffer, 0);
+                    skippedBytes.Enqueue(symbol);
+                    if (written == 0)
+                        continue;
+                    
+                    for (int i = 0; i < written; i++)
+                    {
+                        if (char.IsUpper(charBuffer[i]))
+                        {
+                            list.Add(this._flag);
+                            charBuffer[i] = char.ToLower(charBuffer[i]);
+                            var bytesCount = this.Encoder.GetBytes(charBuffer, 0, 1, lowerCaseBytes, 0, false);
+                            for (int j = 0; j < bytesCount; j++)
+                                list.Add(lowerCaseBytes[j]);
+                            skippedBytes.Clear();
+                        }
+                        else
+                        {
+                            while (skippedBytes.Count != 0)
+                            {
+                                var s = skippedBytes.Dequeue();
+                                if (s == this._flag)
+                                    list.Add(this._flag);
+                                list.Add(s);
+                            }
+                            break;
+                        }
+                    }
+                }
+                yield return list.ToArray();
             }
             this.Decoder.Reset();
             this.Encoder.Reset();
