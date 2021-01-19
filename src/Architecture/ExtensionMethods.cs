15d9302aa6a6138c614770558eefd591e4238410
Mitiko <mitikodev@gmail.com>
2021-01-19 20:42:28 +0200

    Add proper decoder linking.

--- src/Architecture/ExtensionMethods.cs
+++ src/Architecture/ExtensionMethods.cs
@@ -9,11 +9,14 @@ namespace BWDPerf.Architecture
         public static IAsyncEnumerable<ToSymbol> ToCoder<Symbol, ToSymbol>(this ISource<Symbol> source, ICoder<Symbol, ToSymbol> coder) =>
             coder.Encode(source.Fetch());
 
-        public static IAsyncEnumerable<ToSymbol> ToDecoder<Symbol, ToSymbol>(this ISource<Symbol> source, IDecoder<Symbol, ToSymbol> coder) =>
-            coder.Decode(source.Fetch());
-
         public static IAsyncEnumerable<ToSymbol> ToCoder<FromSymbol, ToSymbol>(this IAsyncEnumerable<FromSymbol> pipeline, ICoder<FromSymbol, ToSymbol> coder) =>
             coder.Encode(pipeline);
+
+        public static IAsyncEnumerable<ToSymbol> ToDecoder<Symbol, ToSymbol>(this ISource<Symbol> source, IDecoder<Symbol, ToSymbol> decoder) =>
+            decoder.Decode(source.Fetch());
+
+        public static IAsyncEnumerable<ToSymbol> ToDecoder<FromSymbol, ToSymbol>(this IAsyncEnumerable<FromSymbol> pipeline, IDecoder<FromSymbol, ToSymbol> decoder) =>
+            decoder.Decode(pipeline);
 
         public static async Task Serialize<Symbol>(this IAsyncEnumerable<Symbol> pipeline, ISerializer<Symbol> serializer) =>
             await serializer.Complete(pipeline);
